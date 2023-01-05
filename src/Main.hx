5481049c3b8dfe7bda465aa9df677d778cdbcb38
Sid <saytleader@outlook.com>
2023-01-05 09:28:59 -0500

    add a greeting

--- src/Main.hx
+++ src/Main.hx
@@ -5,5 +5,18 @@ class Main {
 
     public static function main() {
         args = Sys.args();
+
+        if (args.length == 0) {
+            greet();
+            return;
+        }
+
+        return;
+    }
+
+    static function greet() {
+        Sys.stdout().writeString("[V]iew all inventory entries");
+        Sys.stdout().writeString("[E]dit or update the status of an inventory entry");
+        Sys.stdout().writeString("[A]dd a new inventory entry");
     }
 }
